69c2f892a4465242235ceda7e41884576b2f6aff
Thamaraiselvan Kodhandabani <thko2162@stu.sjcc.edu>
2026-04-22 23:00:24 -0700

    made changes to pom file

--- hw/src/main/java/com/egyptianexample/JSONFile.java
+++ hw/src/main/java/com/egyptianexample/JSONFile.java
@@ -1,31 +1,34 @@
-package com.egyptianExample;
+package com.egyptianexample;
 
-import java.io.*;
-import org.json.simple.*;
-import org.json.simple.parser.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JSONFile {
 
   // read a json file and return an array
   public static JSONArray readArray(String fileName) {
-    //
-    // read the birthday.json file and iterate over it
-    //
-
-    // JSON parser object to parse read file
     JSONParser jsonParser = new JSONParser();
-
     JSONArray data = null;
 
-    try (FileReader reader = new FileReader(fileName)) {
-      Object obj = jsonParser.parse(reader);
+    try {
+      InputStream inputStream = JSONFile.class.getClassLoader().getResourceAsStream(fileName);
 
+      if (inputStream == null) {
+        throw new RuntimeException("Could not find file: " + fileName);
+      }
+
+      Reader reader = new InputStreamReader(inputStream);
+      Object obj = jsonParser.parse(reader);
       data = (JSONArray) obj;
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
+      reader.close();
+
     } catch (ParseException e) {
+      e.printStackTrace();
+    } catch (Exception e) {
       e.printStackTrace();
     }
 
